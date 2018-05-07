5db9bc8698d9f6e7aa6d5b11521673b9c93315e4
jiajialingau <jiajialing@gmail.com>
2018-05-07 15:10:07 +1000

    com

---
+++ main.cs
@@ -0,0 +1,20 @@
+
+using System;
+using people;
+
+public class run {
+
+    public static void Main(string[] args)
+        {
+
+            person a = new people.person();
+            a.welcome();
+            a.eating();
+            a.sleep();
+
+        }
+
+
+
+
+}
---
+++ person.cs
@@ -0,0 +1,43 @@
+using System;
+
+
+namespace people
+{
+    public class person
+    {
+        //person constructor
+         string fname;
+         string lname;       
+         int age;
+        public person()
+        {
+           fname="fuck";
+           lname="off";
+           age=100;
+            
+        }
+
+        //person display method
+        public void welcome()
+        {
+            Console.WriteLine("hi, I am "+fname+" "+lname);
+            Console.WriteLine("I am "+age+" years old");
+        }
+
+        public void eating()
+        {
+            Console.WriteLine(fname+" "+lname+" ok, I'm eating now......");
+        }
+
+        public void sleep()
+        {
+            Console.WriteLine(fname+" "+lname+" ok, I'm go to sleep, ZZZZ.........");
+        }
+
+
+
+        //main 
+        
+
+    }
+}
